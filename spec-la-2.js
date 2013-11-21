c64f60fb146cf401a7c90b50caf542ca1bb17386
Michael Matuzak <mmatuzak@gmail.com>
2013-11-20 21:43:57 -0800

    adding 2nd demo

---
+++ spec-la-2.js
@@ -0,0 +1,121 @@
+header({prg: 1, chr: 1, map: 0, mir: 1});
+
+reset();
+vblankwait(1);
+clrmem();
+vblankwait(2);
+
+palette();
+loadSprites();
+
+loadBg();
+loadAttribute();
+
+asm('\tLDA #%10010000');
+asm('\tSTA $2000');
+asm('\tLDA #%00011110');
+asm('\tSTA $2001');
+asm('Forever:');
+asm('\tJMP Forever');
+asm('NMI:');
+
+setLowHighBytes();
+ 
+joyInit();
+joyRead({label: 'ReadA'});
+joyRead({label: 'ReadB'});
+joyRead({label: 'ReadSel'});
+joyRead({label: 'ReadStart'});
+joyRead({label: 'ReadUp', cb: moveUp});
+joyRead({label: 'ReadDown', cb: moveDown});
+joyRead({label: 'ReadLeft', cb: moveLeft});
+joyRead({label: 'ReadRight', cb: moveRight, done: enableBg});
+
+asm('\trti');
+asm('\t.bank 1');
+asm('\t.org $E000');
+
+pData();
+
+addData({x: '80', y: '80', tile: '112', attr: '0'});
+addData({x: '88', y: '80', tile: '113', attr: '0'});
+addData({x: '80', y: '88', tile: '114', attr: '0'});
+addData({x: '88', y: '88', tile: '115', attr: '0'});
+sData();
+
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+addBg('176,178,176,178,176,178,176,178,36,36,36,36,36,36,36,36');
+addBg('36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36');
+addBg('177,179,177,179,177,179,177,179,36,36,36,36,36,36,36,36');
+addBg('36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36');
+write('');
+write('');
+
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+addBg('36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36');
+addBg('36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36');
+addBg('36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36');
+addBg('36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36');
+write('');
+write('');
+
+write('');
+write('');
+write('');
+addBg('36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36');
+addBg('36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36');
+addBg('36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36');
+addBg('36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+write('');
+
+
+loadNametable();
+attributeTable();
+
+asm('\t.org $FFFA');
+
+vec();
+bank(2, '$0000', '\t.incbin "spec-la.chr"');
